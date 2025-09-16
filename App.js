7ef9d456b1ef391a70f8073a9d366d9fef09062c
Feliverse <haybar@gmail.com>
2025-09-16 09:33:59 -0400

    Cambio de color en Background

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#cfc7c7ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
