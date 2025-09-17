aafecaed213aa73ae3cf1ac365cd2d5c7495ac6d
Feliverse <haybar@gmail.com>
2025-09-17 09:03:41 -0400

    Button

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight } from 'react-native';
+
+import Icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image  source={{ uri: "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt8e7b73c3c4bf2f90/657ed8a9cd72b1e4fb2c1ccf/game_features_2.webp?" }} 
+      style={{ 
+        width: 400, 
+        height: 400,
+        borderRadius: 40,
+        }} />
       <Text>Hola Mundo!</Text>
+      <Button title="Press me" onPress={() => alert('Button pressed!')} />
+      <TouchableHighlight onPress={() => alert('Image pressed!')}>
+        <Image source={Icon} style={{ width: 100, height: 100 }} />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
